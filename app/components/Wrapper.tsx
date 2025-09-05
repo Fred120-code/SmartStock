d2df2688b2cbce76d592c8ddebb5ad883dcdaafa
Fred120-code <fredayemtsa@gmail.com>
2025-09-05 14:19:15 +0100

    ajout de resct toastify

---
+++ app/components/Wrapper.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+type WrapperProps = {
+  children: React.ReactNode;
+};
+
+const Wrapper = ({ children }: WrapperProps) => {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      <ToastContainer 
+        position="top-center"
+        autoClose = {500}
+        hideProgressBar={false}
+        newestOnTop={}
+      />
+
+      <div className="px-5 md:px-[10%] mt-8 mb-10">{children}</div>
+    </div>
+  );
+};
+
+export default Wrapper;
