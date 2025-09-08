4fc438ead393dce2fdd71bc03dbc175a04e89831
Fred120-code <fredayemtsa@gmail.com>
2025-09-09 00:57:35 +0100

    traitement des produits

--- app/new_product/page.tsx
+++ app/new_product/page.tsx
@@ -1,13 +1,86 @@
-"use client"
-import React from 'react'
-import Wrapper from '../components/Wrapper'
+"use client";
+import React, { useState } from "react";
+import Wrapper from "../components/Wrapper";
+import { useUser } from "@clerk/nextjs";
+import { Category } from "@prisma/client";
+import { FormDataType } from "@/types";
 
 const page = () => {
+  //importation de l'email de l'utilisateur
+  const { user } = useUser();
+  const email = user?.primaryEmailAddress?.emailAddress as string;
+
+  const [file, setFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [formData, setFormData] = useState<FormDataType>({
+    name: "",
+    description: "",
+    price: 0,
+    categoryId: "",
+    unit: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <Wrapper>
-        test
-    </Wrapper>
-  )
-}
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="font-bold text-2xl mb-4">Creer un produit</h1>
 
-export default page
+          <section className="flex md:flex-row flex-col">
+            <div className="space-y-4 md:w-[450px]">
+              <input
+                type="text"
+                name="name"
+                placeholder="Nom"
+                className="input input-bordered w-full"
+                value={formData.name}
+                onChange={handleChange}
+              />
+
+              <textarea
+                name="description"
+                placeholder="Description"
+                className="textarea textarea-bordered w-full"
+                value={formData.description}
+                onChange={handleChange}
+              ></textarea>
+
+              <input
+                type="number"
+                name="price"
+                placeholder="Prix"
+                className="input input-bordered w-full"
+                value={formData.price}
+                onChange={handleChange}
+              />
+
+              <select
+                className="select selec-bordered"
+                value={formData.categoryId}
+              >
+                <option value="">Selectionner une categorie</option>
+                {categories.map((category) => (
+                  <option value={category.id} key={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default page;
